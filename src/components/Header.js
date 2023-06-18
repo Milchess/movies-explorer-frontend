528370ca70127f6785e282f4b3930f40eeb013fc
Milchess <milch.ks.2992@gmail.com>
2023-06-18 23:09:37 +0500

    Сделано бургер-меню

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,9 +9,6 @@ function Header() {
         <div className='header'>
             <img className="logo" src={headerLogo} alt="Логотип сайта" />
             <nav className="nav">
-                <button className="burger-menu hover-style" type="button">
-                    <img className="burger-menu__img" src={BurgerMenu} alt="Меню бургера" />
-                </button>
                 <nav className="nav__profile">
                     <Link to='/films' className="nav__text hover-style">Фильмы</Link>
                     <Link to='/films' className="nav__text hover-style">Сохранённые фильмы</Link>
@@ -20,6 +17,9 @@ function Header() {
                     <h3 className="account__text">Аккаунт</h3>
                     <img className="account__icon" src={AccountIcon} alt="Значок профиля" />
                 </Link>
+                <button className="burger-menu hover-style" type="button">
+                    <img className="burger-menu__img" src={BurgerMenu} alt="Меню бургера" />
+                </button>
             </nav>
         </div>
     )
