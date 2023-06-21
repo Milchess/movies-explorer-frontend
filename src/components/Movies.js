8ff742c1d7075ac70f2871d6192efadb22c86113
Milchess <milch.ks.2992@gmail.com>
2023-06-21 14:12:49 +0500

    Исправлены картинки в фильмах

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchForm from './SearchForm/SearchForm';
 import MoviesCard from './MoviesCard/MoviesCard';
 import BoxMore from './BoxMore/BoxMore';
+import '../index.css';
 
 function Movies() {
     return (
