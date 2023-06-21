612eb07ccc1151e07453de7ff019f98700f2901c
Milchess <milch.ks.2992@gmail.com>
2023-06-21 12:04:27 +0500

    Ссылки на страницы с проектами портфолио

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -2,22 +2,23 @@ import React from 'react';
 import '../../index.css';
 import './Portfolio.css';
 import pointer from '../../images/pointer.svg';
+import { Link } from 'react-router-dom';
 
 function Portfolio() {
     return (
         <div className='nav-site'>
-            <div className='nav-site__element hover-style'>
+            <Link to={{ pathname: 'https://github.com/Milchess/how-to-learn' }} className='nav-site__element hover-style' target='_blank'>
                 <p className='nav-site__text'>Статичный сайт</p>
                 <img className='nav-site__img' src={pointer} alt='Стрелка перехода'/>
-            </div>
-            <div className='nav-site__element hover-style'>
+            </Link>
+            <Link to={{ pathname: 'https://github.com/Milchess/russian-travel' }} className='nav-site__element hover-style' target='_blank'>
                 <p className='nav-site__text'>Адаптивный сайт</p>
                 <img className='nav-site__img' src={pointer} alt='Стрелка перехода'/>
-            </div>
-            <div className='nav-site__element hover-style'>
+            </Link>
+            <Link to={{ pathname: 'https://github.com/Milchess/react-mesto-api-full' }} className='nav-site__element hover-style' target='_blank'>
                 <p className='nav-site__text'>Одностраничное приложение</p>
                 <img className='nav-site__img' src={pointer} alt='Стрелка перехода'/>
-            </div>
+            </Link>
         </div>
     )
 }
